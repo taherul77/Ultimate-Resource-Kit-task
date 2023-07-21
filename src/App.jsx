f7b3089b8d526e1dfaa0dbf999b549a0e86d1f20
taherul77 <taherul2317@gmail.com>
2023-07-21 17:01:18 +0600

    add mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
-import { Outlet } from "react-router-dom"
-import Nav from "./Components/Nav"
+import { Outlet } from "react-router-dom";
+import Nav from "./Components/Nav";
 
 
 function App() {
-  
-
   return (
     <>
-    <Nav></Nav>
-    <Outlet></Outlet>
-  </>
-  )
+      
+      <Nav></Nav>
+      <Outlet></Outlet>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,32 +1,84 @@
-
-import "./Nav.css";
-import { AiOutlineHome } from "react-icons/ai";
-import { AiOutlineUser } from "react-icons/ai";
-import { FiList } from "react-icons/fi";
-import { BiMessageSquareDetail } from "react-icons/bi";
-import { NavLink } from "react-router-dom";
+import { Box, Stack } from "@mui/material";
+import logo from "../assets/react.svg";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import ArticleIcon from "@mui/icons-material/Article";
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import PersonIcon from '@mui/icons-material/Person';
 
 const Nav = () => {
   return (
     <div>
-        <nav>
-      <NavLink to="/">
-        <AiOutlineHome />
-      </NavLink>
-      <NavLink to="/projects">
-        <FiList />
-      </NavLink>
-      <NavLink to="/contact">
-        <BiMessageSquareDetail />
-      </NavLink>
-      <NavLink to="/about">
-        <AiOutlineUser />
-      </NavLink>
-     
-      
-      
-    </nav>
-    
+      <Box
+        sx={{
+          backgroundColor: "white",
+          boxShadow: "0px 0px 2px rgba(0,0,0,0.025)",
+          height: "100vh",
+          width: 100,
+        }}
+      >
+        <Stack
+          direction="column"
+          alignItems="center"
+          sx={{ alignItems: "center", marginLeft: 2 }}
+        >
+          <Box
+            sx={{
+              backgroundColor: "white",
+              height: 70,
+              width: 70,
+              marginTop: 2,
+              fontSize: 50,
+            }}
+          >
+            <div className="ml-2">
+              <img src={logo} alt="" />
+            </div>
+          </Box>
+          <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+            <SearchIcon
+              color="primary"
+              font:Bolt
+              sx={{ fontSize: 40, marginLeft: 1 }}
+            ></SearchIcon>
+          </Box>
+          <Box
+            sx={{
+              backgroundColor: "white",
+              height: 64,
+              width: 64,
+              marginTop: 2,
+            }}
+          >
+            <HomeIcon
+              color="primary"
+              font:Bolt
+              sx={{ fontSize: 44 }}
+            ></HomeIcon>
+          </Box>
+          <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+            <ArticleIcon
+              color="primary"
+              font:Bolt
+              sx={{ fontSize: 44 }}
+            ></ArticleIcon>
+          </Box>
+          <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+            <ListAltIcon
+              color="disabled"
+              font:Bolt
+              sx={{ fontSize: 44 }}
+            ></ListAltIcon>
+          </Box>
+          <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+            <PersonIcon
+              color="disabled"
+              font:Bolt
+              sx={{ fontSize: 44 }}
+            ></PersonIcon>
+          </Box>
+        </Stack>
+      </Box>
     </div>
   );
 };
