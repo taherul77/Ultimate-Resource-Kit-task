64c71c2317598874c796e6917e72ca3e36251b79
taherul77 <taherul2317@gmail.com>
2023-07-21 18:52:24 +0600

    nav update

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,8 +3,11 @@ import logo from "../assets/react.svg";
 import HomeIcon from "@mui/icons-material/Home";
 import SearchIcon from "@mui/icons-material/Search";
 import ArticleIcon from "@mui/icons-material/Article";
-import ListAltIcon from '@mui/icons-material/ListAlt';
-import PersonIcon from '@mui/icons-material/Person';
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PersonIcon from "@mui/icons-material/Person";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
@@ -20,63 +23,105 @@ const Nav = () => {
         <Stack
           direction="column"
           alignItems="center"
-          sx={{ alignItems: "center", marginLeft: 2 }}
+          justifyContent="space-between"
+          sx={{ height: "100%" }}
+          spacing={2}
         >
-          <Box
-            sx={{
-              backgroundColor: "white",
-              height: 70,
-              width: 70,
-              marginTop: 2,
-              fontSize: 50,
-            }}
+          <Stack alignItems="center" spacing={1}>
+            <Link to="/">
+            <Box
+              sx={{
+                backgroundColor: "white",
+                height: 70,
+                width: 70,
+                marginTop: 2,
+                fontSize: 50,
+              }}
+            >
+              <div className="ml-2">
+                <img src={logo} alt="" />
+              </div>
+            </Box>
+            
+            </Link>
+            
+            <Link>
+            <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+              <SearchIcon
+                color="disabled"
+                font:Bolt
+                sx={{ fontSize: 40, marginLeft: 1 }}
+              ></SearchIcon>
+            </Box>
+            </Link>
+            <Link>
+            <Box
+              sx={{
+                backgroundColor: "white",
+                height: 64,
+                width: 64,
+                marginTop: 2,
+              }}
+            >
+              <HomeIcon
+                color="primary"
+                font:Bolt
+                sx={{ fontSize: 44 }}
+              ></HomeIcon>
+            </Box>
+            </Link>
+            <Link>
+            <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+              <ArticleIcon
+                color="disabled"
+                font:Bolt
+                sx={{ fontSize: 44 }}
+              ></ArticleIcon>
+            </Box>
+            </Link>
+            <Link>
+            <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+              <ListAltIcon
+                color="disabled"
+                font:Bolt
+                sx={{ fontSize: 44 }}
+              ></ListAltIcon>
+            </Box>
+            </Link>
+            <Link>
+            <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+              <PersonIcon
+                color="disabled"
+                font:Bolt
+                sx={{ fontSize: 44 }}
+              ></PersonIcon>
+            </Box>
+            </Link>
+            
+            
+            
+            
+           
+          </Stack>
+          <Stack
+            alignItems="center" spacing={2}
           >
-            <div className="ml-2">
-              <img src={logo} alt="" />
-            </div>
-          </Box>
-          <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
-            <SearchIcon
-              color="disabled"
-              font:Bolt
-              sx={{ fontSize: 40, marginLeft: 1 }}
-            ></SearchIcon>
-          </Box>
-          <Box
-            sx={{
-              backgroundColor: "white",
-              height: 64,
-              width: 64,
-              marginTop: 2,
-            }}
-          >
-            <HomeIcon
-              color="primary"
-              font:Bolt
-              sx={{ fontSize: 44 }}
-            ></HomeIcon>
-          </Box>
-          <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
-            <ArticleIcon
+            <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+            <NotificationsNoneIcon
               color="disabled"
               font:Bolt
               sx={{ fontSize: 44 }}
-            ></ArticleIcon>
-          </Box>
-          <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
-            <ListAltIcon
+            ></NotificationsNoneIcon>
+            </Box>
+            <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
+            <LogoutIcon
               color="disabled"
               font:Bolt
               sx={{ fontSize: 44 }}
-            ></ListAltIcon>
-          </Box>
-          <Box sx={{ backgroundColor: "white", height: 64, width: 64 }}>
-            <PersonIcon
-              color="disabled"
-              font:Bolt
-              sx={{ fontSize: 44 }}
-            ></PersonIcon>
-          </Box>
+            ></LogoutIcon>
+            </Box>
+           
+          </Stack>
         </Stack>
       </Box>
     </div>
